a865f3cfa3c892db8dbe75c5f8ebb6e74d5a8c44
Yash Gupta <btech10430.24@bitmesra.ac.in>
2025-06-06 14:47:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+    const scrollLine = document.querySelector('.scroll-line');
+
+    function updateScrollProgress() {
+      const windowHeight = window.innerHeight;
+      const fullHeight = document.body.clientHeight;
+      const scrolled = window.scrollY;
+      const percentScrolled = (scrolled / (fullHeight - windowHeight)) * 100;
+      scrollLine.style.width = `${percentScrolled}%`;
+    }
+
+    updateScrollProgress();
+
+    let ticking = false;
+    window.addEventListener('scroll', () => {
+      if (!ticking) {
+        window.requestAnimationFrame(() => {
+          updateScrollProgress();
+          ticking = false;
+        });
+        ticking = true;
+      }
+    });
+
+    window.addEventListener('resize', updateScrollProgress);
