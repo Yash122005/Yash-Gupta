ab3a0beff1e886236eb16761037aae11ed7ec181
Yash Gupta <btech10430.24@bitmesra.ac.in>
2025-06-12 17:05:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,19 @@
     });
 
     window.addEventListener('resize', updateScrollProgress);
+
+
+    gsap.from("#skills-logo img",{
+  opacity:0,
+  duration:1,
+  x:-90,
+  stagger:1,
+  scrollTrigger:{
+    trigger:"#skills-logo",
+    scroller:"body",
+    start:"top 80%",
+    end:"top 30%",
+    markers:true,
+    scrub:5
+  }
+  })
