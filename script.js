33dcc88ebe50080d9d787a5c2c81746e926a97ca
Yash Gupta <btech10430.24@bitmesra.ac.in>
2025-06-30 23:21:57 +0530

    Update script.js
    mouse fallower

--- script.js
+++ script.js
@@ -22,6 +22,13 @@
     });
 
     window.addEventListener('resize', updateScrollProgress);
+// mouse fallower
+Shery.mouseFollower({
+  //Parameters are optional.
+  skew: true,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
 
 
     gsap.from("#skills-logo img",{
